aa87db54e8ad0b768ae647f1c716fd024f818971
RoggerParedesTavara <rparedestavara@gmail.com>
2021-10-20 16:33:30 -0300

    Creado componente Loader

---
+++ src/components/LoaderComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ActivityIndicator, Modal, View } from 'react-native'
+import generalStyle from '../styles/generalStyle'
+
+export const LoaderComponent = () => {
+    return (
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={true}>   
+            <View style={{flex: 1, alignItems: 'center', 
+            justifyContent: 'center', backgroundColor: 'rgba(0,0,0,0.5)'}}>
+                <ActivityIndicator style={generalStyle.loader} size="large" 
+                color="rgba(255, 255, 255, 0.7)" />
+            </View>
+        </Modal>
+    )
+}
